1ba09e1308ebf13e919dd211077b3baa36941a08
Kawan Silva <99221604+ksilva-kwn@users.noreply.github.com>
2023-11-21 11:35:52 -0300

    Update TRABALHO DO CARLOS.cpp

--- TRABALHO DO CARLOS.cpp
+++ TRABALHO DO CARLOS.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stdio.h>
 #include <locale.h>
 
 using namespace std;
 
 struct Associado {
-    int codigo;
-    string tipo_socio, cpf, nome, dia_associacao, mes_associacao, ano_associacao,;
+    int codigo, qntdDependentes;
+    string nome, cpf, dia_associacao,mes_associacao,ano_associacao, tipo_socio, data_nasc;
     float mensalidade;
     vector<int> dependentes;
 };
@@ -18,190 +19,281 @@ struct Dependente {
 };
 
 struct Visitante {
-    int codigo, codigo_associado;
-    string nome, cpf, data_nasc, sexo, telefone, email, endereco, data_visita_inicial, data_visita_final;
+    int codigo, cpf, codigo_associado;
+    string nome, data_nasc, sexo, data_visita_inicial, data_visita_final;
 };
 
-Associado cadastroassociado;
-Dependente cadastrodependente;
-Visitante cadastrovisitante;
+Associado cadastrarAssociadoStruct;
+Dependente cadastrarDependenteStruct;
+Visitante cadastrarVisitanteStruct;
 
-void carregarDadosDoArquivo();
-void salvarDadosDependentesEmArquivo();
+int contadorDeLinhas(const string& nomeArquivo);
+void cadastrarAssociado();
+void cadastrarDependente();
+void cadastrarVisitante();
+void avisoMaioridade();
+void registrarVisita();
+void relatorioDependentesAssociados();
+void relatorioVisitasPorAssociado();
 void salvarDadosAssociadosEmArquivo();
+void salvarDadosDependentesEmArquivo();
+void salvarDadosVisitantesEmArquivo();
+void carregarDadosDoArquivo();
+
+
+vector<Associado>associados;
+vector<Dependente>dependentes;
+vector<Visitante>visitantes;
+
+int main() {
+	setlocale(LC_ALL,"portuguese");
+	
+    carregarDadosDoArquivo();
+
+    char escolha;
+    do {
+        cout << "\n------ Menu ------\n";
+        cout << "1. Cadastrar associado\n";
+        cout << "2. Cadastrar dependente\n";
+        cout << "3. Cadastrar visitante\n";
+        cout << "4. Aviso automático de maioridade\n";
+        cout << "5. Registro de visita\n";
+        cout << "6. Relatório de dependentes/associados\n";
+        cout << "7. Relatório de visitas por associado\n";
+        cout << "8. Alterar Dados\n";
+        cout << "9. Encerrar o Programa\n";
+        cout << "Escolha uma opção (1-9): ";
+        cin >> escolha;
+        cout << endl;
+
+        switch (escolha) {
+            case '1':
+                cadastrarAssociado();
+                break;
+            case '2':
+                cadastrarDependente();
+                break;
+            case '3':
+                cadastrarVisitante();
+                break;
+            case '4':
+                avisoMaioridade();
+                break;
+            case '5':
+                registrarVisita();
+                break;
+            case '6':
+                relatorioDependentesAssociados();
+                break;
+            case '7':
+                relatorioVisitasPorAssociado();
+                break;
+            case '8':
+                
+                break;
+            case '9':
+            	cout << "Programa encerrado.\n";
+            	break;
+            default:
+                cout << "Opção inválida. Tente novamente.\n";
+        }
+    } while (escolha != '9');
+
+    return 0;
+}
+
+int contadorDeLinhas(const char* nomeArquivo) {
+    FILE *arquivo;
+    int contador = 0;
+    char linha[100000];
+
+    // Abre o arquivo para leitura
+    arquivo = fopen(nomeArquivo, "r");
+
+    // Verifica se o arquivo foi aberto corretamente
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo: %s\n", nomeArquivo);
+        return -1; // Retorna -1 para indicar um erro
+    }
+
+    // Lê cada linha do arquivo
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        contador++;
+    }
+
+    // Fecha o arquivo
+    fclose(arquivo);
+
+    return contador;
+}
 
 void cadastrarAssociado() {
-	
-	// 1- Cadastrar Associado. O getline é usado para aceitar espaço entre as palavras.
-	cout << endl;
-	cin.ignore();
-	
-	cout << "Digite o CPF: ";
-	getline(cin, cadastroassociado.cpf);
+  	
+	const char* nomeArquivo = "dadosAssociados.txt";
+  	int linhas = contadorDeLinhas(nomeArquivo);
+  	
+  	cout << "Digite o nome do associado (Utilize ' _ 'como espaço): ";
+  	cin >> cadastrarAssociadoStruct.nome;
+  	
+  	cout << "Digite o CPF: ";
+	cin >> cadastrarAssociadoStruct.cpf;
 	
 	cout << endl;
 	
 	cout << "Insire a data de Associação: ";
 	cout << endl;
+	
 	cout << "Dia: ";
-	getline(cin, cadastroassociado.dia_associacao);
-	cout << endl;
+	cin >> cadastrarAssociadoStruct.dia_associacao;
+	
 	cout << "Mes: ";
-	getline(cin, cadastroassociado.mes_associacao);
-	cout << endl;
+	cin >> cadastrarAssociadoStruct.mes_associacao;
+	
 	cout << "Ano: ";
-	getline(cin, cadastroassociado.ano_associacao);
+	cin >> cadastrarAssociadoStruct.ano_associacao;
 	
 	cout << endl;
 	
 	cout << "Insira o tipo de sócio (Proprietário ou Contribuinte): ";
-	getline(cin,  cadastroassociado.tipo_socio);
+	cin >> cadastrarAssociadoStruct.tipo_socio;
 	
 	cout << endl;
-	
-	cadastroassociado.codigo = cadastroassociado.codigo + 1;
-	cout << "O codigo do Associado é: " << cadastroassociado.codigo;
-	
-	cout << endl;
-	
-	associados.push_back(cadastroassociado);
+
+  	cadastrarAssociadoStruct.codigo = linhas + 1;
+  	cout << "O codigo do Associado é: " << cadastrarAssociadoStruct.codigo;
+  	
+  	associados.push_back(cadastrarAssociadoStruct);
 	salvarDadosAssociadosEmArquivo();
+	
+	cout << endl;
+	cout << endl;
+	
+	cout << "Digite quantos dependentes você quer cadastrar: ";
+	cin >> cadastrarAssociadoStruct.qntdDependentes;
+	cout << endl;
+	
+	for(int i=0; i < cadastrarAssociadoStruct.qntdDependentes; i++){
+		cadastrarDependente();
+	}
 }
 
 void cadastrarDependente() {
-    // 2- Cadastrar dependentes
-    cin.ignore();
-    cout << endl;
+	
+	const char* nomeArquivo = "dadosDependentes.txt";
+  	int linhas = contadorDeLinhas(nomeArquivo);
     
-    cout << "Insira o nome: ";
-    getline(cin, cadastrodependente.nome);
-    
-    cout << endl;
-    
-    cout << "Insira a data de nascimento: ";
-    getline(cin, cadastrodependente.data_nasc);
+    cout << "Insira o nome(Utilize ' _ ' como espaço): ";
+    cin >> cadastrarDependenteStruct.nome;
     
     cout << endl;
     
-    cout << "Insira o sexo (M  ou F): ";
-    getline(cin, cadastrodependente.sexo);
+    cout << "Insira a data de nascimento (Utilize / ou - e 0): ";
+    cin >> cadastrarDependenteStruct.data_nasc;
     
     cout << endl;
     
-    cadastrodependente.codigo = cadastrodependente.codigo + 1;
-    cout << "O codigo do Dependente é: " << cadastrodependente.codigo;
+    cout << "Insira o sexo (Masculino  ou Feminino): ";
+    cin >> cadastrarDependenteStruct.sexo;
     
-    dependentes.push_back(cadastrodependente);
+    cout << endl;
     
-    salvarDadosDependentesEmArquivo();
+    cadastrarDependenteStruct.codigo = linhas + 1;
+    cout << "O codigo do Dependente é: " << cadastrarDependenteStruct.codigo;
+    
+    dependentes.push_back(cadastrarDependenteStruct);
+	salvarDadosDependentesEmArquivo();
 }
 
-// 3- Cadastrar visitante.
 void cadastrarVisitante() {
-    
-    cout << endl;
-    cout << "Digite seu nome: ";
-	cin.ignore();
-    getline(cin, cadastrovisitante.nome);
-    
-    cout << endl;
-    
-    cout << "Digite seu CPF: ";
-    getline(cin, cadastrovisitante.cpf);
-    
-    cout << endl;
-    
-    cout << "Digite sua data de nascimento: ";
-    getline(cin, cadastrovisitante.data_nasc);
-    
-	cout << endl;
-    
-	cout << "Digite seu endereço: ";
-	getline(cin, cadastrovisitante.endereco);
+	
+	const char* nomeArquivo = "dadosVisitantes.txt";
+  	int linhas = contadorDeLinhas(nomeArquivo);
+  	
+	cout << "Insira o nome do Visitante(Utilize ' _ ' como espaço): ";
+	cin >> cadastrarVisitanteStruct.nome;
 	
 	cout << endl;
 	
-	cout << "Digite seu sexo (M ou F): ";
-	getline(cin, cadastrovisitante.sexo);
+	cout << "Insira a data de nascimento (Utilize / ou - e 0): ";
+	cin >> cadastrarVisitanteStruct.data_nasc;
 	
 	cout << endl;
 	
-	cout << "Digite seu telefone (com DDD): ";
-	getline(cin, cadastrovisitante.telefone);
+	cout << "Insira o sexo (Masculino  ou Feminino): ";
+	cin >> cadastrarVisitanteStruct.sexo;
 	
 	cout << endl;
 	
-	cout << "Digite seu E-mail: ";
-	getline(cin, cadastrovisitante.email);
+	cout << "Insira a data de visita inicial ( Utilize - ou / ): ";
+	cin >> cadastrarVisitanteStruct.data_visita_inicial;
 	
 	cout << endl;
 	
-	cout << "Insira a data de Visita Inicial: ";
-	getline(cin, cadastrovisitante.data_visita_inicial);
+	cout << "Insira a data de visita final ( Utilize - ou / ): ";
+	cin >> cadastrarVisitanteStruct.data_visita_final;
 	
 	cout << endl;
 	
-	cout << "Insira a data de Visita Final: ";
-	getline(cin, cadastrovisitante.data_visita_final);
+	cadastrarVisitanteStruct.codigo = linhas+1;
+	cout << "O código do visitante é: " << cadastrarVisitanteStruct.codigo;
 	
-	cout << endl;
 	
-	cadastrovisitante.codigo = cadastrovisitante.codigo + 1;
-	cout << "O codigo do Visitante é: " << cadastrovisitante.codigo;
-	
+	visitantes.push_back(cadastrarVisitanteStruct);
+	salvarDadosVisitantesEmArquivo();
 }
 
 void avisoMaioridade() {
-    // Implemente a lógica de aviso de maioridade
+    
 }
 
 void registrarVisita() {
-    // Implemente a lógica de registro de visita
+    
 }
 
 void relatorioDependentesAssociados() {
-    // Implemente a lógica de relatório de dependentes/associados
+    
 }
 
 void relatorioVisitasPorAssociado() {
-    // Implemente a lógica de relatório de visitas por associado
+    
 }
 
+
 void salvarDadosAssociadosEmArquivo(){
-	 ofstream arquivoA("dadosAssociados.txt", ios::app); // Abre o arquivo em modo de anexação
-    if (arquivoA.is_open()) {
+	ofstream arquivo("dadosAssociados.txt");
+    if (arquivo.is_open()) {
         for (size_t i = 0; i < associados.size(); ++i) {
             const Associado& associado = associados[i];
-            arquivoA << associado.codigo << " " << associado.dia_associacao << " " << associado.mes_associacao << " " << associado.ano_associacao << " " << associado.tipo_socio << " " << associado.cpf << " " << associado.nome << " " << associado.mensalidade << "\n";
+            arquivo <<"codigo:"<< associado.codigo << " " <<"tipo:"<< associado.tipo_socio << " " <<"cpf:" << associado.cpf << " " <<"diaA:"<< associado.dia_associacao << " " <<"mesA:" <<associado.mes_associacao << " " <<"anoA:"<< associado.ano_associacao << " "<<"mensalidade:"<< associado.mensalidade << " " << "nome:"<<associado.nome << "\n";
         }
-        arquivoA.close();
+        arquivo.close();
     } else {
         cout << "Erro ao abrir o arquivo de associados." << endl;
     }
 }
 
 void salvarDadosDependentesEmArquivo(){
-	ofstream arquivoD("dadosDependentes.txt", ios::app);
-    if (arquivoD.is_open()) {
+	ofstream arquivo("dadosDependentes.txt");
+    if (arquivo.is_open()) {
         for (size_t i = 0; i < dependentes.size(); ++i) {
             const Dependente& dependente = dependentes[i];
-            arquivoD << dependente.codigo << " " << dependente.codExterno << " " << dependente.cpf_associado << " " << dependente.nome << " " << dependente.data_nasc << " " << dependente.sexo << "\n";
+            arquivo <<"codigo:"<< dependente.codigo << " " << dependente.codExterno << " " << dependente.cpf_associado << " " << dependente.nome << " " << dependente.data_nasc << " " << dependente.sexo << "\n";
         }
-        arquivoD.close();
+        arquivo.close();
     } else {
         cout << "Erro ao abrir o arquivo de dependentes." << endl;
     }
 }
 
 void salvarDadosVisitantesEmArquivo(){
-	ofstream arquivoV("dadosVisitante.txt", ios::app);
-    if (arquivoV.is_open()) {
+	ofstream arquivo("dadosVisitantes.txt");
+    if (arquivo.is_open()) {
         for (size_t i = 0; i < visitantes.size(); ++i) {
             const Visitante& visitante = visitantes[i];
-            arquivoV << visitante.codigo << " " << visitante.codigo_associado << " " << visitante.nome << " " << visitante.cpf << " " << visitante.data_nasc << " " << visitante.sexo << " "
-                     << visitante.telefone << " " << visitante.email << " " << visitante.endereco << " " << visitante.data_visita_inicial << " " << visitante.data_visita_final << "\n";
+            arquivo << visitante.codigo << " " << visitante.cpf << " " << visitante.codigo_associado << " " << visitante.nome << " " << visitante.data_nasc << " " << visitante.sexo << " "
+                    << visitante.data_visita_inicial << " " << visitante.data_visita_final << "\n";
         }
-        arquivoV.close();
+        arquivo.close();
     } else {
         cout << "Erro ao abrir o arquivo de visitantes." << endl;
     }
@@ -212,54 +304,4 @@ void carregarDadosDoArquivo() {
     arquivoA.open("dadosAssociados.txt", fstream::in|fstream::out|fstream::app);
     arquivoD.open("dadosDependentes.txt", fstream::in|fstream::out|fstream::app);
     arquivoV.open("dadosVisitante.txt", fstream::in|fstream::out|fstream::app);
-}
-
-int main() {
-	setlocale(LC_ALL,"portuguese");
-    carregarDadosDoArquivo();
-
-    int escolha;
-    do {
-        cout << "\n------ Menu ------\n";
-        cout << "1. Cadastrar associado\n";
-        cout << "2. Cadastrar dependente\n";
-        cout << "3. Cadastrar visitante\n";
-        cout << "4. Aviso automático de maioridade\n";
-        cout << "5. Registro de visita\n";
-        cout << "6. Relatório de dependentes/associados\n";
-        cout << "7. Relatório de visitas por associado\n";
-        cout << "8. Encerrar programa\n";
-        cout << "Escolha uma opção: ";
-        cin >> escolha;
-
-        switch (escolha) {
-            case 1:
-                cadastrarAssociado();
-                break;
-            case 2:
-                cadastrarDependente();
-                break;
-            case 3:
-                cadastrarVisitante();
-                break;
-            case 4:
-                avisoMaioridade();
-                break;
-            case 5:
-                registrarVisita();
-                break;
-            case 6:
-                relatorioDependentesAssociados();
-                break;
-            case 7:
-                relatorioVisitasPorAssociado();
-                break;
-            case 8:
-                break;
-            default:
-                cout << "Opção inválida. Tente novamente.\n";
-        }
-    } while (escolha != 8);
-
-    return 0;
 }
